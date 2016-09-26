e00c64b779bbc6dae597b202a657b24c581d8cdd
VisualGhost <weatherrocket@gmail.com>
2016-09-26 18:59:19 +0300

    Add billing module

---
+++ billing/src/test/java/com/billing/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.billing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
